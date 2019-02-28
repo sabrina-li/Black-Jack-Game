c1a2f6cd02cbc95e9425ddb717f456d77d49fd44
Sabrina L <sabrinali0312@gmail.com>
2019-02-27 22:34:32 -0500

    add files and build html

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+//GAME START
+
+var playChips = 10;
+//player start with 10 chips, each round is 1 chip
+//TODO let player choose number of chips
+
+//function animate the move from deck to player
+function animate(destinationDiv){
+    let cardBack = $("#deck").clone();
+    
+
+}
+//function(numOfCards,destinationDiv) generate 1 or 2 randome number for player card, show the cards to player
+//show button to hit or stand
+
+//hit.click generate new number and animate, show card
+    //bust?
+    
+
+//stand.click dealer get 2 cards
+    //generate 
+    //decide hit or stand
+    //roundresult?
+
+//round result? 
+    //player win - get dealer's chip
+    //dealer win - player looses the chips
+
+})
